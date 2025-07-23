7444c8414bb7db1061803888af9aefbecfea4bb9
Ar_Sen1x <bncmanliguez03174@usep.edu.ph>
2025-07-23 11:05:51 +0800

    change location to display ilunas logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,6 +1,6 @@
 <a href="/" class="flex items-center space-x-2">
     <!-- Logo Image -->
-    <img src="/images/ilunas_logo.png" alt="iLunas Logo" class="h-10 w-auto">
+    <img src="{{ asset('assets/ilunas_logo.png') }}" alt="iLunas Logo" class="h-10 w-auto">
 
     <!-- Website Name -->
     <span class="text-lg font-semibold text-white">
