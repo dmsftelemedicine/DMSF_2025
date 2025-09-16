ea663816c483562caf5fb6510eda0f81424615d9
Inkrypth <edwincali2002@gmail.com>
2025-09-16 15:18:34 +0800

    fix(user): route on top of page

--- resources/views/patients/screeningtool/forms/nutrition_tab.blade.php
+++ resources/views/patients/screeningtool/forms/nutrition_tab.blade.php
@@ -4,7 +4,7 @@
 	<div class="row">
 		<div class="col-4 mb-3">
 			<p class="text-muted mb-1">BMR (kcal/day)</p>
-			<p class="fw-bold">{{ $patient->tdee->bmr }}</p>
+			<p class="fw-bold">{{ optional($patient->tdee)->bmr ?? 'N/A' }}</p>
 		</div>
 		<div class="col-4 mb-3">
 			<p class="text-muted mb-1">
