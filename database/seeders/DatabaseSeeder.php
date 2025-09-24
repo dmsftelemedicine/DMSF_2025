53c4a833f93360fb03961c2693d590fb716a8c55
ZaitonZz <philipcandelario1@gmail.com>
2025-09-24 15:09:50 +0800

    feat(seeder): add test user creation in DatabaseSeeder if not exists

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +21,14 @@ class DatabaseSeeder extends Seeder
             Icd10Seeder::class,
             MedicineSeeder::class,
         ]);
+        if (!User::where('email', 'test@example.com')->exists()) {
+            User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+                'first_name' => 'Test',
+                'last_name' => 'User',
+                'role' => 'admin',
+            ]);
+        }
     }
 }
